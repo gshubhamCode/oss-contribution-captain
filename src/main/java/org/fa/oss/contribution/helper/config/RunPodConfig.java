99eded808d32a797ac1271de0d2a117b598af4e6
Shubham Gupta <shubham18p@gmail.com>
2025-06-28 16:08:00 +0530

    add: runpod manager utilities

---
+++ src/main/java/org/fa/oss/contribution/helper/config/RunPodConfig.java
@@ -0,0 +1,18 @@
+package org.fa.oss.contribution.helper.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "runpod")
+@Getter
+@Setter
+public class RunPodConfig {
+
+
+    private String apiKey;
+    private String vllmApiKey;
+    private String podId;
+}
